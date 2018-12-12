53dcc1bddabb6d29413aacb11dbbf2cb48cab652
Sidratul Muntaha <smuntaha97@yahoo.com>
2018-12-12 23:11:16 +0600

    Unit Test Policeview

--- project/server/test/policeTest.js
+++ project/server/test/policeTest.js
@@ -118,10 +118,26 @@ describe('/POST INSERT Police with No Data', () => {
  /*
         Test the /GET route for one Police
     */
-   describe('/GET VIEW Police', () => {
+   describe('/GET VIEW One Police', () => {
     it('it should return an object of the Police requested by the admin', (done) => {
         chai.request(server)
         .get('/api/police/view/:pid')
+        //set admin token
+        .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imt3MzYxc2pwOHF1dHdkIiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJ1c2VyX3R5cGUiOjMsImlhdCI6MTU0NDM3NjU1MSwiZXhwIjoxNTQ0MzgwMTUxfQ.tLmwYfg3meXaMPhVtc5Vq9cZpGE87fVXG7HkdSEzIjo')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+ /*
+        Test the /GET route for viewing Police Profile
+    */
+   describe('/GET VIEW Police Profile', () => {
+    it('it should return an object of the Police requested by the admin', (done) => {
+        chai.request(server)
+        .get('/api/police/editprofile')
         //set admin token
         .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Imt3MzYxc2pwOHF1dHdkIiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJ1c2VyX3R5cGUiOjMsImlhdCI6MTU0NDM3NjU1MSwiZXhwIjoxNTQ0MzgwMTUxfQ.tLmwYfg3meXaMPhVtc5Vq9cZpGE87fVXG7HkdSEzIjo')
         .end((err, res) => {
